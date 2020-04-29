503cbb6c67d9b0732c59c183fea5aac366d71f37
gajanansanap22992 <gajanansanap22992@gmail.com>
2020-04-29 18:13:41 +0530

    commit spring boot product crudapi

---
+++ ProductAPI/src/main/java/com/gajanan/restapi/ProductController.java
@@ -0,0 +1,52 @@
+package com.gajanan.restapi;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.*;
+ 
+import org.springframework.web.bind.annotation.*;
+ 
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+ 
+    @Autowired
+    private ProductService service;
+    
+    @GetMapping("/list")
+    public List<Product> list() {
+        return service.listAll();
+    }
+    
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Product> get(@PathVariable Integer id) {
+        try {
+            Product product = service.get(id);
+            return new ResponseEntity<Product>(product, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<Product>(HttpStatus.NOT_FOUND);
+        }      
+    }
+    @PostMapping("/create")
+    public String add(@RequestBody Product product) {
+        service.save(product);
+        return "record added successfully";
+    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@RequestBody Product product, @PathVariable Integer id) {
+        try {
+            Product existProduct = service.get(id);
+            service.save(product);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }      
+    }
+    
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        service.delete(id);
+        return "record deleted successfully";
+    }
+}
